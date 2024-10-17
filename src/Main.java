9f2583e27a30b15e46266ce935cfdd1985362ad2
Valentine0604 <ewecia.s@gmail.com>
2024-10-17 18:53:34 +0200

    feat: add registration panel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello friend, welcome to CatHub!");
+        System.out.println("Click here to register!");
+        Registration registration = new Registration("Leon", "Pink Ball", "6", "Maine Coon", "Ewelina");
     }
 }
